8faec9658656f62241ab3be43fa3110ce2715ece
Juan Montoya <juanmontoya015@gmail.com>
2023-11-20 22:05:50 -0500

    [FEATURE]: Actualización Script BD
     - Se realiza la actualización del script a la base de datos con un banco de información para la API.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -28,3 +28,49 @@ CREATE TABLE IF NOT EXISTS comentario (
     texto VARCHAR(255),
     fecha VARCHAR(255)
 );
+
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Casa de la Cultura', 'Calle 24 # 8-55', 'Bogotá');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Colón', 'Calle 10 # 5-32', 'Bogotá');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Jorge Eliécer Gaitán', 'Carrera 7 # 22-47', 'Bogotá');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Mayor Julio Mario Santo Domingo', 'Calle 170 # 67-51', 'Bogotá');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Nacional Fanny Mikey', 'Calle 71 # 10-25', 'Bogotá');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Pablo Tobón Uribe', 'Carrera 40 # 51-24', 'Medellín');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Metropolitano', 'Carrera 41 # 57-30', 'Medellín');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Lido', 'Carrera 46 # 51-15', 'Medellín');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Pablo Tobón Uribe', 'Carrera 40 # 51-24', 'Medellín');
+INSERT INTO ubicacion (nombre, direccion, ciudad) VALUES ('Teatro Metropolitano', 'Carrera 41 # 57-30', 'Medellín');
+
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Juan', 'juan@email.com', 18, true);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Pedro', 'pedro@email.com', 23, true);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Alberto', 'alberto@email.com', 23, true);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Sandra', 'sandra@email.com', 26, false);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Alejandra', 'alejandra@email.com', 28, false);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Ana', 'ana@email.com', 21, false);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Maria', 'maria@email.com', 30, false);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Fernando', 'fernando@email.com', 32, true);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Michelle', 'michelle@email.com', 22, false);
+INSERT INTO usuario (nombre, correo, edad, genero) VALUES ('Oscar', 'oscar@email.com', 28, true);
+
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de rock', 'Concierto de rock en el teatro', '2021-10-10', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de salsa', 'Concierto de salsa en el teatro', '2021-10-11', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de jazz', 'Concierto de jazz en el teatro', '2021-10-12', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de pop', 'Concierto de pop en el teatro', '2021-10-13', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de reggaeton', 'Concierto de reggaeton en el teatro', '2021-10-14', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de vallenato', 'Concierto de vallenato en el teatro', '2021-10-15', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de merengue', 'Concierto de merengue en el teatro', '2021-10-16', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de cumbia', 'Concierto de cumbia en el teatro', '2021-10-17', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de reggae', 'Concierto de reggae en el teatro', '2021-10-18', 100);
+INSERT INTO evento (titulo, descripcion, fecha, capacidad) VALUES ('Concierto de bachata', 'Concierto de bachata en el teatro', '2021-10-19', 100);
+
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (1, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (2, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (3, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (4, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (5, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (6, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (7, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (8, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (9, 1, 'Me encanta el rock', '2021-10-10');
+INSERT INTO comentario (autor, evento, texto, fecha) VALUES (10, 1, 'Me encanta el rock', '2021-10-10');
+
+
